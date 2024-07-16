57fb24b1b5af46ef625142e01fe2544d3bed58ea
levanduandut <fbsinon300101@gmail.com>
2024-07-16 11:46:16 +0700

    Initial Commit

---
+++ I am very RichUITests/I_am_very_RichUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  I_am_very_RichUITestsLaunchTests.swift
+//  I am very RichUITests
+//
+//  Created by GN on 16/07/2024.
+//
+
+import XCTest
+
+final class I_am_very_RichUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
